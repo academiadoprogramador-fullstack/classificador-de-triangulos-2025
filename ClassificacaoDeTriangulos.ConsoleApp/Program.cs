77ad6fa2e0725705ae2f9c0179b44b89b2a90f23
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-18 15:39:26 -0300

    Implementada versão inicial com menu

--- ClassificacaoDeTriangulos.ConsoleApp/Program.cs
+++ ClassificacaoDeTriangulos.ConsoleApp/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while(true)
+            {
+                // exibir o cabeçalho
+                Console.Clear();
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine("Classificador de Triângulos");
+                Console.WriteLine("-----------------------------------------------");
+
+                // receber a entrada (input) de dados
+                Console.Write("Informe o valor do lado X: ");
+                int ladoX = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Informe o valor do lado Y: ");
+                int ladoY = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Informe o valor do lado Z: ");
+                int ladoZ = Convert.ToInt32(Console.ReadLine());
+
+                // exibir os valores digitados
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine("Valores informados:");
+                Console.WriteLine("Lado X: " + ladoX);
+                Console.WriteLine("Lado Y: " + ladoY);
+                Console.WriteLine("Lado Z: " + ladoZ);
+
+                Console.WriteLine("-----------------------------------------------");
+                Console.Write("Deseja continuar? (s/N) ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
         }
     }
 }
